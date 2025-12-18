fb114e21fd2803acc3d2a18b0f773199a0cba351
Harry Merzin <harry@basepowercompany.com>
2025-12-17 22:05:52 -0600

    wip

--- src/address-search/UtilityModal.tsx
+++ src/address-search/UtilityModal.tsx
@@ -34,6 +34,7 @@ export function UtilityModal({
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
           >
             <path
               d="M19 12H5M5 12L12 19M5 12L12 5"
