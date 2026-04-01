cea09644174f62d8ace55942c64b5c2b77b9d6f1
divazbozz <davidbozzman@gmail.com>
2026-04-01 16:17:11 -0500

    build

--- src/address-search/modal/AddressConfirmModal.tsx
+++ src/address-search/modal/AddressConfirmModal.tsx
@@ -1,31 +1,11 @@
 import { useCallback, useEffect, useId, useRef, useState } from "react";
+import { CloseIcon } from "@/address-search/icons/CloseIcon";
 import type {
 	AddressResult,
 	ParsedGoogleAddressComponents,
 } from "@/address-search/types";
 import { cx } from "@/utils/cx";
 import styles from "./styles.module.css";
-
-function CloseIcon() {
-	return (
-		<svg
-			width="20"
-			height="20"
-			viewBox="0 0 24 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-			aria-hidden="true"
-		>
-			<path
-				d="M18 6L6 18M6 6l12 12"
-				stroke="currentColor"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			/>
-		</svg>
-	);
-}
 
 export type AddressConfirmModalProps = {
 	selection: AddressResult;
